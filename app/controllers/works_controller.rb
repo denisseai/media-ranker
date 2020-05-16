572fb0b320bca9b9525f588efc1a55712f8de9ed
denisseai <msdenisseanaya@gmail.com>
2020-05-15 21:05:22 -0700

    Added views show.html.erb and method show

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -4,4 +4,13 @@ class WorksController < ApplicationController
     @albums = Work.where(category: 'album')
     @books = Work.where(category: 'book')
   end
+
+  def show
+    @work = Work.find_by(id: params[:id])
+    if @work.nil?
+      head :not_found
+      return
+    end
+  end
+
 end
