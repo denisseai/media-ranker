df0c40d143f4cdb6d5976c9b9a29e12420328777
denisseai <msdenisseanaya@gmail.com>
2020-05-15 20:33:46 -0700

    Login button and Logout button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,4 +21,20 @@ class UsersController < ApplicationController
     session[:user_id] = user.id
     redirect_to root_path
   end
+
+  def logout
+    if session[:user_id]
+      user= User.find_by(id: session[:user_id])
+      unless user.nil?
+        session[:user_id] = nil
+        flash[:notice] = "Goodbye, #{user.username}"
+      else
+        session[:user_id] = nil
+        flash[:notice] = "Error Unkonwn User"
+      end
+    else
+      flash[:error] = "You must be logged in to logout"
+    end
+    redirect_to root_path
+  end
 end
