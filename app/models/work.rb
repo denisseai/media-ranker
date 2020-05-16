13ba3b5e6ab9446d54333388211aab017c7b0ec7
denisseai <msdenisseanaya@gmail.com>
2020-05-16 15:20:00 -0700

    Added start to spotlight in views index for home d

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,13 +1,13 @@
 class Work < ApplicationRecord
-  def all_categories
-    unique_categories = []
-    self.all.each do |work|
-      if !unique_categories.include?(work.category)
-        unique_categories << work.category
-      end
-    end
-    return unique_categories
-  end
+  # def all_categories
+  #   unique_categories = []
+  #   self.all.each do |work|
+  #     if !unique_categories.include?(work.category)
+  #       unique_categories << work.category
+  #     end
+  #   end
+  #   return unique_categories
+  # end
 
   def self.top_books
     books = Work.where(category: "book")
@@ -17,6 +17,10 @@ class Work < ApplicationRecord
   def self.top_albums
     albums = Work.where(category: "album")
     return albums.sample(10)
+  end
+
+  def self.top_media
+    return Work.all.sample
   end
 
 end
