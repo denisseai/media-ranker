eab111b9c791e5a0234ed6fd202bcaf04a74b0a5
denisseai <msdenisseanaya@gmail.com>
2020-05-16 22:56:39 -0700

    Added authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,14 +8,14 @@ class UsersController < ApplicationController
 
     if user.nil?
       user = User.new(username: params[:user][:username])
-      if ! user.save
+      if !user.save
         flash[:error] = "Unable to login"
         redirect_to root_path
         return
       end
-      flash[:welcome] = "Welcome, #{user.username}"
+      flash[:welcome] = "Welcome #{user.username}"
     else
-      flash[:welcome] = "Welcome back, #{user.username}"
+      flash[:welcome] = "Welcome back #{user.username}"
     end
 
     session[:user_id] = user.id
@@ -25,9 +25,10 @@ class UsersController < ApplicationController
   def logout
     if session[:user_id]
       user= User.find_by(id: session[:user_id])
+
       unless user.nil?
         session[:user_id] = nil
-        flash[:notice] = "Goodbye, #{user.username}"
+        flash[:notice] = "Goodbye #{user.username}"
       else
         session[:user_id] = nil
         flash[:notice] = "Error Unkonwn User"
@@ -36,5 +37,14 @@ class UsersController < ApplicationController
       flash[:error] = "You must be logged in to logout"
     end
     redirect_to root_path
+  end
+
+  def current
+    @user = User.find_by(id: session[:user_id])
+    if @user.nil?
+      flash[:error] = "You must be logged in to view this page"
+      redirect_to root_path
+      return
+    end
   end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -50,4 +50,23 @@ describe UsersController do
       expect(session[:user_id]).must_be_nil
     end
   end
+
+  describe "current user" do
+    it "can return the page if user is logged in" do
+      # Arrange
+      login()
+      # Act
+      get current_user_path
+      # Assert
+      must_respond_with :success
+    end
+
+    it "redirects us back if the user is not logged in" do
+      # Act
+      get current_user_path
+      # Assert
+      must_respond_with :redirect
+      expect(flash[:error]).must_equal "You must be logged in to view this page"
+    end
+  end
 end
