70892e7533bccd3de50f4725130d8d6f3e304945
denisseai <msdenisseanaya@gmail.com>
2020-05-18 21:31:23 -0700

    User name display when login and out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,37 +1,37 @@
 class UsersController < ApplicationController
   def index
-    @user = User.all
+    user = User.all
   end
 
   def show
-    @user = User.find_by(id: params[:id])
-    if @user.nil?
+    user = User.find_by(id: params[:id])
+    if user.nil?
       head :not_found
       return
     end
   end
 
   def login_form
-    @user = User.new
+    user = User.new
   end
 
   def login
-    @user = User.find_by(username: params[:user][:username])
+    user = User.find_by(username: params[:user][:username])
 
-    if @user.nil?
+    if user.nil?
       date = Time.now
-      @user = User.new(username: params[:user][:username], , join_date: date.strftime("%B %d, %Y").to_s)
+      user = User.new(username: params[:user][:username])
       if !user.save
         flash[:error] = "Unable to login"
         redirect_to root_path
         return
       end
-      flash[:welcome] = "Welcome #{@user.username}"
+      flash[:welcome] = "Welcome #{user.username}"
     else
-      flash[:welcome] = "Welcome back #{@user.username}"
+      flash[:welcome] = "Welcome back #{user.username}"
     end
 
-    session[:user_id] = @user.id
+    session[:user_id] = user.id
     redirect_to root_path
   end
 
